05368306a41de20f60278e7aa2813717f0fc6c19
pavan142 <pa1tirumani@gmail.com>
2021-03-13 08:27:50 +0530

    Hid the navigation bar and made the status bar transparent
    Signed-off-by: pavan142 <pa1tirumani@gmail.com>

--- app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
+++ app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
@@ -21,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private void hideSystemUI() {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         _decorView.setSystemUiVisibility(uiOptions);
     }
 
