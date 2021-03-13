7d01e87bbc140965d01aa84f43502dc993635461
pavan142 <pa1tirumani@gmail.com>
2021-03-13 08:51:03 +0530

    Added support for tap to collapse navigation bar 1. Also changed the background to blue
    Signed-off-by: pavan142 <pa1tirumani@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+## An android application with collapsible navigation bars for a full screen activity
--- app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
+++ app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
@@ -10,12 +10,32 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private  View _decorView;
+    private GestureDetector _tapDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         _decorView = getWindow().getDecorView();
         hideSystemUI();
         setContentView(R.layout.activity_main);
+        _tapDetector = new GestureDetector(this,
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapUp(MotionEvent e) {
+                        boolean visible = (_decorView.getSystemUiVisibility() & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0;
+                        if (visible)
+                            hideSystemUI();
+                        else
+                            showSystemUI();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        _tapDetector.onTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
     }
 
     private void hideSystemUI() {
@@ -23,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        _decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    private void showSystemUI() {
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         _decorView.setSystemUiVisibility(uiOptions);
     }
