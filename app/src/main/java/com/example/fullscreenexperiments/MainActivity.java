28e3f51d400d70bb9ab706ed30b5d280878de5e0
pavan142 <pa1tirumani@gmail.com>
2021-03-13 08:19:26 +0530

    Removed the status bar from view
    Signed-off-by: pavan142 <pa1tirumani@gmail.com>

--- app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
+++ app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
@@ -8,9 +8,24 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  View _decorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        _decorView = getWindow().getDecorView();
+        hideSystemUI();
         setContentView(R.layout.activity_main);
+    }
+
+    private void hideSystemUI() {
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        _decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    protected void onResume() {
+        hideSystemUI();
+        super.onResume();
     }
 }
