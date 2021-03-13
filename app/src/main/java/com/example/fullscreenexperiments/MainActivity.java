f85e8ed5f2de5c129fb7dd8588f152d6a3abc4f1
pavan142 <pa1tirumani@gmail.com>
2021-03-13 08:11:22 +0530

    [First Commit] Default Implementation of Empty Activity by AndroidStudio
    Signed-off-by: pavan142 <pa1tirumani@gmail.com>

---
+++ app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fullscreenexperiments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
