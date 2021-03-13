346fb87d9215b44210b450b6c98170859dc5356d
pavan142 <pa1tirumani@gmail.com>
2021-03-13 12:24:38 +0530

    Added comments for the responsiblities of visibility flags
    Signed-off-by: pavan142 <pa1tirumani@gmail.com>

--- app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
+++ app/src/main/java/com/example/fullscreenexperiments/MainActivity.java
@@ -38,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
         return super.dispatchTouchEvent(ev);
     }
 
+    // SYSTEM_UI_FLAG_FULLSCREEN // Hide the status bar
+    // SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Let the layout expand into status bar
+    // SYSTEM_UI_FLAG_LAYOUT_STABLE // avoid abrupt layout changes during toggling of status and navigation bars
+    // SYSTEM_UI_FLAG_HIDE_NAVIGATION // Hide the navigation bar
+    // SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION; // Let the layout expand into navigation bar
+
     private void hideSystemUI() {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
